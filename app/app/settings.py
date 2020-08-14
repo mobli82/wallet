af555f0262cb69a0ae181ff9afeb4acee9c19eb4
mobli <moblixxyy@gmail.com>
2020-08-14 12:04:57 +0200

    added postgresql in settings.py
    create databse migrations and superuser

--- app/app/settings.py
+++ app/app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-z3)rv#9#523s#w^d(lxc9rjd=oq09zhei+1x!i!-$f-m!7mc0'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'wallet',
+        'PORT': '5432',
+        'HOST': 'localhost',
+        'USERNAME': os.environ.get('USER'),
+        'PASSWORD': os.environ.get('PSQL'),
     }
 }
 
