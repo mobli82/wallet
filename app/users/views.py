602c5d9432db8305d83982b121314b9c2b8239b7
mobli <moblixxyy@gmail.com>
2020-08-14 15:32:11 +0200

    created UserRegistartionView in  users/views.py
    added path for registration view in users/urls.py
    
    created registration template in users/templates
    
    created base.html template
    
    added CRISPY_TEMPLTE_PACK and crispy_forms in app/settings.py

--- app/users/views.py
+++ app/users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from .forms import UserRegistrationForm
 # Create your views here.
+
+def register(request):
+    form = UserRegistrationForm(request.POST or None,)
+
+    if form.is_valid():
+        form.save()
+        return redirect('login')
+
+    return render(request, 'users/registration.html', {'form': form})
