6eb20bde63c12bbefdc7d805de55263fd4e40977
mobli <moblixxyy@gmail.com>
2020-08-17 19:04:57 +0200

    created wallet app for manage base.html and nav_bar.html
    changed path for base.html and nav_bar.html

--- app/app/settings.py
+++ app/app/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'crispy_forms',
     'users.apps.UsersConfig',
+    'wallet.apps.WalletConfig',
 ]
 
 MIDDLEWARE = [
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'wallet/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- app/app/urls.py
+++ app/app/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', include('users.urls')),
+    path('', include('wallet.urls')),
 ]
