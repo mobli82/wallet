fdb9f66aa4c4a32c49bb468aedb90a1848fb7fb0
mobli <moblixxyy@gmail.com>
2020-08-18 09:10:15 +0200

    transffered base.html and nav_bar.html to main templates folder
    edited path in all html templates and template dir in setting.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'wallet/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
