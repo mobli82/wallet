0096eb92e697d4cac77f37d68934299bf0c5124d
mobli <moblixxyy@gmail.com>
2020-08-20 17:10:23 +0200

    created users password reset templates/password_reset.html 				      /password_reset_complete.html 				      /password_reset_confirm.html 				      /password_reset_done.html
    added email configs in app/settings.py
    
    added urls paths for reset password templates
    
    added links for reset password route in users/login.html template
    
    created legend in users/registration.html and user/login.html template

--- app/app/settings.py
+++ app/app/settings.py
@@ -135,3 +135,10 @@ LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = '/profile/'
 
 CRISPY_TEMPALTE_PACK = 'bootstrap4'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL')
+EMAIL_HOST_PASSWORD = os.environ.get('DJANGO_PASS')
--- app/app/urls.py
+++ app/app/urls.py
@@ -13,7 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import  (LoginView, 
+                                        LogoutView, 
+                                        PasswordResetView,
+                                        PasswordResetDoneView,
+                                        PasswordResetConfirmView,
+                                        PasswordResetCompleteView,
+                                        )
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -23,6 +29,15 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('password-reset/', PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
+    path('password-reset/done/', PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), 
+                                                            name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(
+                                                                template_name='users/password_reset_confirm.html'), 
+                                                                name='password_reset_confirm'),
+    path('password-reset-complete/', PasswordResetCompleteView.as_view(
+                                                                template_name='users/password_reset_complete.html'), 
+                                                                name='password_reset_complete'),
     path('', include('users.urls')),
     path('', include('wallet.urls')),
 ] 
