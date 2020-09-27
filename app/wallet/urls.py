01892b01c386c257f1a49ff5039d38427d8f7dad
mobli <moblixxyy@gmail.com>
2020-09-27 12:50:02 +0200

    created simple javascript plots for gaz_counter app and power_counter app
    created gaz_costs_stats.js
    created gaz_usage_stats.js
    created power_costs_stats.js
    created power_usage_stats.js
    
    added gaz_counter_summary_costs function in wallet/views.py
    added power_counter_summary_usage function in wallet/views.py
    added power_counter_summary_costs function in wallet/views.py
    renemed gaz_counter_summary_usage in wallet/views.py
    
    added paths in wallet/urls.py for javascript functions views

--- app/wallet/urls.py
+++ app/wallet/urls.py
@@ -1,10 +1,21 @@
 from django.urls import path
-from .views import home, counters_list, statistics, gaz_counter_summary, suppliers_list
+from .views import (home, 
+                    counters_list, 
+                    statistics, 
+                    gaz_counter_summary_usage, 
+                    gaz_counter_summary_costs, 
+                    suppliers_list,
+                    power_counter_summary_usage,
+                    power_counter_summary_costs,
+)
 
 urlpatterns = [
     path("", home, name="home"),
     path("countres/", counters_list, name="counters"),
     path("statistics-list/", statistics, name="statistics-list"),
-    path("gaz-summary/", gaz_counter_summary, name="gaz-summary"),
+    path("gaz-summary-usage/", gaz_counter_summary_usage, name="gaz-summary-usage"),
+    path("gaz-summary-costs/", gaz_counter_summary_costs, name="gaz-summary-costs"),
+    path("power-summary-usage/", power_counter_summary_usage, name="power-summary-usage"),
+    path("power-summary-costs/", power_counter_summary_costs, name="power-summary-costs"),
     path("suppliers-list/", suppliers_list, name="suppliers-list"),
 ]
